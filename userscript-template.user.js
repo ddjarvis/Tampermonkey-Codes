c1231c95bf3905ad16c949e75216335ca4ca21e0
David Jarvis <ddjarvis@up.edu.ph>
2022-11-04 17:40:42 +0800

    Update userscript-template.user.js

--- userscript-template.user.js
+++ userscript-template.user.js
@@ -1,1 +1,25 @@
+// ==UserScript==
+// @name         userscript-template
+// @author       ddjarvis
+// @version      0.1
+// @updateURL    
+// @downloadURL  
+// @namespace    http://tampermonkey.net/
+// @description  try to take over the world!
+// @match        http://example.com
+// @icon         https://www.google.com/s2/favicons?domain=example.com
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addCode-1.0.2/js-functions/addCode.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addCss-1.0.1/js-functions/addCss.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addElement-1.3.0/js-functions/addElement.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@copyText-1.0.0/js-functions/copyText.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@fileSafe-1.0.0/js-functions/fileSafe.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@getElement-1.1.0/js-functions/getElement.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@parseURL-1.0.0/js-functions/parseURL.js
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
