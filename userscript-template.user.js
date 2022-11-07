e3aac5ccfe1ef823fba7cde6a208256009e4879b
David Jarvis <ddjarvis@up.edu.ph>
2022-11-07 11:07:00 +0800

    Update userscript-template.user.js

--- userscript-template.user.js
+++ userscript-template.user.js
@@ -1,25 +1,42 @@
 // ==UserScript==
-// @name         userscript-template
-// @author       ddjarvis
-// @version      0.1
-// @updateURL    
-// @downloadURL  
+// @name         {userscript-template}
 // @namespace    http://tampermonkey.net/
+// @version      0.1
 // @description  try to take over the world!
+// @author       ddjarvis
 // @match        http://example.com
 // @icon         https://www.google.com/s2/favicons?domain=example.com
-// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addCode-1.0.2/js-functions/addCode.js
-// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addCss-1.0.1/js-functions/addCss.js
-// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addElement-1.3.0/js-functions/addElement.js
 // @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@copyText-1.0.0/js-functions/copyText.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addElement-1.3.0/js-functions/addElement.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addCss-1.0.1/js-functions/addCss.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@parseURL-1.0.0/js-functions/parseURL.js
+// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@addCode-1.0.2/js-functions/addCode.js
 // @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@fileSafe-1.0.0/js-functions/fileSafe.js
 // @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@getElement-1.1.0/js-functions/getElement.js
-// @require      https://cdn.jsdelivr.net/gh/ddjarvis/JS-Snippets@parseURL-1.0.0/js-functions/parseURL.js
 // @grant        none
 // ==/UserScript==
+
+/* == JS Functions == */
+//  copyText.js				count: 23	used: 21
+//  addElement.js			count: 25	used: 20
+//  addCss.js				count: 23	used: 18
+//  parseURL.js				count: 12	used: 11
+//  addCode.js				count: 12	used: 10
+//  fileSafe.js				count: 3	used: 3
+//  getElement.js			count: 3	used: 1
+
+// Github: https://github.com/ddjarvis/Tampermonkey-Codes/blob/main/userscript-template.user.js
 
 (function() {
     'use strict';
 
+
     // Your code here...
+/*
+ const counter = () => {
+var query = document.querySelectorAll('.filter_text')[0].value;
+var len = Array.from(document.querySelectorAll('.scripttable > tbody > .scripttr')).filter(tr => (tr.style.display != 'none') && !(tr.querySelector('span[title^="{userscript-template}"]')) ).length;
+console.log(`Query: ${query} [${len}]\n`);
+}
+*/
 })();
